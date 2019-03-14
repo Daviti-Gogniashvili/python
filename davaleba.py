68eb3c62395f9b12ca0568bda151f971e49c9a24
Daviti-Gogniashvili <44542496+Daviti-Gogniashvili@users.noreply.github.com>
2019-03-14 14:52:49 +0400

    Update davaleba.py

--- davaleba.py
+++ davaleba.py
@@ -1,23 +1,17 @@
-def check(array):
-  raw = open("samples.txt", "w")
+def check(array, new_list):
+  for i in array:
+        if type(i) == int:
+          new_list.append(i)
+        else:
+          check(i, new_list)
 
-  for dg in array:
-    try:
-      if dg.isdigit():
-        writefile = raw.write(dg)
-    except AttributeError:
-      check(str(dg))
-  raw.close()
+    
 
 def main():
   multi_dimensional_array = [1, [9, [[[[[2]]]]]], [[[2], [12, [2]], [10]]]]
-  raw_list = [multi_dimensional_array]
-  check(raw_list)
   new_list = []
-  raw_file = open("samples.txt", "r")
-  readfile = raw_file.read()
-  for i in readfile:
-      new_list.append(int(i))
+  check(multi_dimensional_array,new_list)
   print(new_list)
 
-main()
+if __name__ == "__main__":
+  main()
